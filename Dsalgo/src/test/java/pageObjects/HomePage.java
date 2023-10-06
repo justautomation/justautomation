b6ea8446e8ab0c9136a1061b73e608bbb9bca013
justautomation21 <justautomation21@gmail.com>
2023-10-06 09:52:38 -0700

    commit 4

--- Dsalgo/src/test/java/pageObjects/HomePage.java
+++ Dsalgo/src/test/java/pageObjects/HomePage.java
@@ -1,5 +1,7 @@
 package pageObjects;
 
+import static org.testng.Assert.assertEquals;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
@@ -20,17 +22,40 @@ public class HomePage extends BaseClass {
 	By dataStructureDropDwn = By.xpath("//a[contains(text(), 'Data Structures')]");
 	By getStartedButton = By.xpath("//a[contains(@href, 'data-structures-introduction')]");
 	By errorMessage = By.xpath("//div[contains(text(), 'You are not logged in')]");
+	By navDropDwn = By.xpath("//div[@class='nav-item dropdown']/a");
+	By arrayLink = By.xpath("//div[@class='dropdown-menu show']/a[1]");
+	By registerLink = By.xpath("//div[@class ='navbar-nav']/ul/a[contains(text(),'Register')]");
 
 	public boolean isDropdownDisplayed() {
 		return driver.findElement(dataStructureDropDwn).isDisplayed();
 	}
 
-	public void getStartedButtonClick() {
+	public void getStartedButtonClick(String getStartedBtn) {
+		String getBtn = driver.findElement(getStartedButton).getText();
+		assertEquals(getBtn, getStartedBtn);
 		Helper.webClick(driver.findElement(getStartedButton));
 	}
 
-	public boolean verifyErrorMessage() {
-		this.driver = Helper.getDriver();
-		return driver.findElement(errorMessage).isDisplayed();
+	public void selectDropdownOption() {
+		driver.findElement(navDropDwn).click();
+		driver.findElement(arrayLink).click();
+	}
+
+	public String verifyErrorMessage() {
+		return driver.findElement(errorMessage).getText();
+	}
+
+	public void clickRegisterBtn(String register) {
+		String registerBtn = driver.findElement(registerLink).getText();
+		assertEquals(registerBtn, register);
+		driver.findElement(registerLink).click();
+
+	}
+
+	public String verifyRegisterPage() {
+		return driver.getCurrentUrl();
+//		public String getHomePageTitle() {
+//			return driver.findElement(homePageTitle).getText();
+//		}
 	}
 }
